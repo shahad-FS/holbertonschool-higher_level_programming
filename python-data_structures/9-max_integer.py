8aa1b1fa09d1563432dcd8a55639e7e1e513ec4d
shahad-FS <alsaneea.s@gmail.com>
2025-11-30 14:37:00 +0000

    max function

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    i = 0
-    max_num = 0
-    while i < len(my_list):
-        if my_list[i] + 1 > len(my_list):
-            break
-        elif my_list[i] < my_list[i + 1]:
-            max_num =  my_list[i + 1]
-        elif my_list[i] > my_list[i + 1]:
-            max_num = my_list[i]
-        i += 1
+    if not my_list:
+        return None
+    max_num = my_list[0]
+    for number in my_list:
+        if number > max_num:
+            max_num = number
     return (max_num)

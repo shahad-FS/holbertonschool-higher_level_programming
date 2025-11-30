19bd1847ef30a12d18a9f60b9740ec3c1ed9e323
shahad-FS <alsaneea.s@gmail.com>
2025-11-30 11:52:55 +0000

    Fix: error to none

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return -1
-    elif idx > len(my_list):
-        return -1
+    if idx < 0 or idx >= len(my_list):
+        return None
     else:
         value = my_list[idx]
     return value

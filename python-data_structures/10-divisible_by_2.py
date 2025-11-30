9a0b332ba95e88644c7e9f7709865b1bd4216424
shahad-FS <alsaneea.s@gmail.com>
2025-11-30 14:47:17 +0000

    divisible_by_2 Function

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for number in my_list:
-        if number % 2 == 0 :
+        if number % 2 == 0:
             value = True
             new_list.append(value)
         else:

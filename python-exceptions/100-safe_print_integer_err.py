93ea5f520f9738e5d69d444ee0939528c687f6cb
shahad-FS <alsaneea.s@gmail.com>
2025-12-02 13:30:55 +0000

    safe_print_integer_err function

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".foramt(value))
+        return True
+    except (TypeError, ValueError) as tv:
+        print("Exception: {}".format(tv), file=sys.stderr)
+        return False

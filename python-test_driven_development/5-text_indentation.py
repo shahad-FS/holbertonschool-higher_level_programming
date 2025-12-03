7d318d416be6f8334c1d9662842d86020090d10c
shahad-FS <alsaneea.s@gmail.com>
2025-12-03 14:47:53 +0000

    text_indentation function and its doctest

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module contains text_indentation function"""
+
+def text_indentation(text):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    Args:
+        text: string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in ['.', '?', ':']:
+        text = text.replace(char, char + "\n\n")
+    print(text.strip())

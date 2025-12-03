823010d3bf05f9ae2ceed2717f1321ea3dbaac12
shahad-FS <alsaneea.s@gmail.com>
2025-12-03 13:50:34 +0000

    matrix_divided function and its unit test documentation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,7 +16,7 @@ def matrix_divided(matrix, div):
 
     for array in matrix:
         if not isinstance(array, list):
-            raise TypeError("matrix must be a matrix (list of lists) of " 
+            raise TypeError("matrix must be a matrix (list of lists) of "
                             "integers/floats")
         if len(array) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")

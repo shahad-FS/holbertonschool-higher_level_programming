7e69b1d8ad20a3e5b4ab2261180d79bd57bc468d
shahad-FS <alsaneea.s@gmail.com>
2025-12-03 12:54:11 +0000

    add_integer function and its own test unit file documentations

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,11 +12,11 @@ def add_integer(a, b=98):
     Returns:
         The Sum of the two integers
     """
-    if not isinstance(a, float | int):
+    if not isinstance(a, (float, int)):
         if a is not None:
             raise TypeError("a must be an integer")
         raise ValueError("a must be an integer")
-    if not isinstance(b, float | int):
+    if not isinstance(b, (float, int)):
         if b is not None:
             raise TypeError("b must be an integer")
         raise ValueError("b must be an integer")

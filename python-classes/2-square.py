4f8b69d161fa4843fff67ef9d3a4b5ace3011857
shahad-FS <alsaneea.s@gmail.com>
2025-12-09 10:32:30 +0000

    raise error if size is not an integer and not greater or equal to zero

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """
+        The __init__ method initialize instance variable
+
+        Attributes:
+        size : size of a square Private instance attribute
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+

475e5e1604f4dd2f35649f56c02befe139daa59e
shahad-FS <alsaneea.s@gmail.com>
2025-12-09 14:06:47 +0000

    create my_print method

---
+++ python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """
+        The __init__ method initialize instance variable
+
+        Attributes:
+        size : size of a square Private instance attribute
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter to retrieve square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to set square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size**2
+
+    def my_print(self):
+        """print in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+            return
+        for row in range(self.__size):
+            for column in range(self.__size):
+                print("#", end="")
+            print("")

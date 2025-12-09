1e64e41271079aff6a81034d8e711fa466fc10f1
shahad-FS <alsaneea.s@gmail.com>
2025-12-09 13:58:23 +0000

    Getter and Setter was established

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -26,7 +26,7 @@ class Square:
     @size.setter
     def size(self, value):
         """Setter to set square size"""
-        if not instance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")

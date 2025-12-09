81f44514bea42b2aac5dedecac14ee71820052db
shahad-FS <alsaneea.s@gmail.com>
2025-12-09 13:57:13 +0000

    Getter and Setter was established

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,6 +11,11 @@ class Square:
         Attributes:
         size : size of a square Private instance attribute
         """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
 
     @property

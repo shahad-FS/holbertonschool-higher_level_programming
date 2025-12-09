95c0db3f6bfd04314df1697f64bb31cded9d0b99
shahad-FS <alsaneea.s@gmail.com>
2025-12-09 13:54:18 +0000

    Getter and Setter was established

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,7 +13,6 @@ class Square:
         """
         self.__size = size
 
-    
     @property
     def size(self):
         """Getter to retrieve square size"""
@@ -27,7 +26,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         """Return the current square area"""
         return self.__size**2

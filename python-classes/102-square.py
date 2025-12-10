c0127ef2f5e4d4e597be73dfc0883498ebada459
shahad-FS <alsaneea.s@gmail.com>
2025-12-10 21:09:18 +0000

    Compare 2 square methods

---
+++ python-classes/102-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0):
+        """
+        The __init__ method initialize instance variable
+
+        Attributes:
+        size : size of a square Private instance attribute
+        """
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter to retrieve square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to set square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size**2
+
+    """comparison operators based on area"""
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area >= other.area()

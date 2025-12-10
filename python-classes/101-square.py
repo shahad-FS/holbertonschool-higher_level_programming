e5c239e26eec1dba8dac70d6b1720145a252ac47
shahad-FS <alsaneea.s@gmail.com>
2025-12-10 21:01:57 +0000

    print square instance

---
+++ python-classes/101-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        The __init__ method initialize instance variable
+
+        Attributes:
+        size : size of a square
+        position : position of the square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter to retrieve square size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Getter to retrieve square position"""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """Setter to set square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Setter to set square position"""
+        if not (isinstance(value, tuple) and len(value) == 2 and
+                all(isinstance(num, int) for num in value) and
+                all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size**2
+
+    def my_print(self):
+        """print in stdout the square with the character #"""
+        if self.size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end='')
+            for row in range(self.__size):
+                print(self.__position[0] * " " + self.size * "#")
+
+    def __str__(self):
+        """Return the string representation of the square"""
+        if self.size == 0:
+            return ""
+        lines = []
+        for i in range(self.__position[1]):
+            lines.append("")
+        for row in range(self.__size):
+            lines.append(self.__position[0] * " " + self.size * "#")
+        return "\n".join(lines)

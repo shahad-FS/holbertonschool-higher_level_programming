1166b481014a2ee979668d48c9976d1228095a4e
shahad-FS <alsaneea.s@gmail.com>
2025-12-14 20:19:21 +0000

    create str method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module contains class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Initialize width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """public instance method that returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """return the rectangle description"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __str__(self):
+        """return the square description"""
+        return f"[Square] {self.__size}/{self.__size}"

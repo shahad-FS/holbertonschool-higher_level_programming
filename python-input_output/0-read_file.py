d7f04b17cf1c8a76a5cebe4b4a037ca84294e0c5
shahad-FS <alsaneea.s@gmail.com>
2025-12-21 11:15:51 +0000

    read file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Moduel for read file function"""
 
 
 def read_file(filename=""):
-    """Function that reads a text file (UTF8) and prints it to stdout"""
+    """
+    Function that reads a text file (UTF8) and prints it to stdout
+    """
     with open("filename", encoding="utf-8") as f:
         read_file = f.read()
         print(read_file)

91668609be77537b01c8a6aae529bf05dd653897
shahad-FS <alsaneea.s@gmail.com>
2025-12-21 17:01:32 +0000

    add_item secript

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module for add_items script"""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file

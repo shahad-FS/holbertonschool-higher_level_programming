f8f27f3bf6c6703f6bd2d46284ad01542ba263f0
shahad-FS <alsaneea.s@gmail.com>
2025-12-21 17:34:35 +0000

    Student class

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -15,5 +15,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return{key, value for key, value in self.__dict__.items()
+            return{key: value for key, value in self.__dict__.items()
                     if key in attrs}

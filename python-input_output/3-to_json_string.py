c43ee7c52186b2082b15e815ddebf1748adb98bf
shahad-FS <alsaneea.s@gmail.com>
2025-12-21 15:37:34 +0000

    to_json_string function

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for to_json_string function"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Docstring for to_json_string
+    Function that returns the JSON representation
+    of an object (string)
+    """
+    return json.dumps(my_obj)

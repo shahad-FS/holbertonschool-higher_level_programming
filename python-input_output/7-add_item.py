9601c9cc645630c06d79d429d7ad3ad6d0225579
shahad-FS <alsaneea.s@gmail.com>
2025-12-21 17:07:59 +0000

    add_item secript

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Module for add_items script"""
-import sys
+"""Module for add argument to python list script"""
+from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -8,11 +9,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 file_name = "add_item.json"
 
 try:
-    load_from_json_file(file_name)
-except Exception:
-    items = []
+    list = load_from_json_file(file_name)
+except FileNotFoundError:
+    list = []
 
-for arg in sys.argv[1:]:
-    items.append(arg)
+list.extend(argv[1:])
 
 save_to_json_file(items, file_name)

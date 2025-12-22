45f1cd4ac471fe72cef43bc0ede966f5bc4fc0f1
shahad-FS <alsaneea.s@gmail.com>
2025-12-22 20:49:26 +0000

    custom class

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""Module for serializing and deserializing using pickle"""
+import pickle
+
+
+class CustomObject:
+    """A custom object to demonstrate serialization and deserialization"""
+    def __init__(self, name, age, is_student):
+        """Initialize the CustomObject"""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the object's attributes"""
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+    def serialize(self, filename):
+        """Serialize the object to a file"""
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Deserialize an object from a file"""
+        with open(filename, "rb") as file:
+            return pickle.load(file)

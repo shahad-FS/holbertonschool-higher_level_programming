b0f64b982d4f50c88e5da35c64d44df2662add3c
shahad-FS <alsaneea.s@gmail.com>
2025-12-27 11:02:45 +0000

    log parsing script

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -3,6 +3,13 @@
 import sys
 
 
+def print_stats():
+    """Function that prints the statistics."""
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
+
 total_size = 0
 line_count = 0
 status_codes = {
@@ -16,35 +23,30 @@ status_codes = {
     '500': 0
 }
 
-def print_stats():
-    """Function that prints the statistics."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
-
-
 try:
     for line in sys.stdin:
-        line_count += 1
+        if line_count != 0 and line_count % 10 == 0:
+            print_stats()
 
         data = line.split()
-        if len(data) < 2:
-            continue
+        try:
+            status_code = int(data[-2])
 
-        status_code = data[-2]
-        file_size = int(data[-1])
+            if str(status_code) in status_codes:
+                status_code[str(status_code)] += 1
+        except:
+            pass
 
-        total_size += file_size
+        try:
+            file_size += int(data[-1])
 
+        except:
+            pass
 
-        if status_code in status_codes:
-            status_codes[status_code] += 1
+        line_count += 1
 
-        if line_count % 10 == 0:
-            print_stats()
-except (IndexError, ValueError):
-    pass
+    print_stats()
+
 except KeyboardInterrupt:
     print_stats()
     raise

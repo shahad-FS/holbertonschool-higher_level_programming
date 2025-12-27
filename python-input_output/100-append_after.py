7e9fa9c7610c16a07875c82984b4ec6ffdd2139e
shahad-FS <alsaneea.s@gmail.com>
2025-12-27 09:51:29 +0000

    append_after function

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+'''Module for append_after function.'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''Function that inserts a line of text to a file,
+    after each line containing a specific string'''
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)

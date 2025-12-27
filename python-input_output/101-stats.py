7b1f8a1a2cf04ce7eeaceca81af078eb33a775f5
shahad-FS <alsaneea.s@gmail.com>
2025-12-27 11:32:40 +0000

    make all .py excutable

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -6,8 +6,6 @@ Reads stdin line by line and computes metrics:
 Prints statistics every 10 lines and on keyboard interruption.
 """
 
-
-
 def print_stats():
     """Function that prints the statistics."""
     print(f"File size: {total_size}")
@@ -15,21 +13,20 @@ def print_stats():
         if status_codes[code] > 0:
             print(f"{code}: {status_codes[code]}")
 
-
 if __name__ == "__main__":
     import sys
 
     total_size = 0
     line_count = 0
     status_codes = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
     }
 
     try:
@@ -46,14 +43,16 @@ if __name__ == "__main__":
 
             total_size += file_size
             line_count += 1
+
             if status_code in status_codes:
                 status_codes[status_code] += 1
+
             if line_count % 10 == 0:
                 print_stats()
-
 
     except KeyboardInterrupt:
         print_stats()
         raise
+
     if line_count % 10 != 0:
         print_stats()

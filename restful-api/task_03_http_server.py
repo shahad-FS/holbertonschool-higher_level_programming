a556a26bb2b9434c26ba81b46c220755385a54d7
shahad-FS <alsaneea.s@gmail.com>
2025-12-29 12:21:13 +0000

    simple API

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -30,7 +30,7 @@ class Server(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write("FAIL".encode())
+            self.wfile.write("404 Not Found".encode())
 
 server = http.server.HTTPServer(("", PORT), Server)
 print("Server is running...")

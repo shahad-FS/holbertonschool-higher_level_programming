366eaf49103df3be32e904e530ba0a51bd2fd03b
shahad-FS <alsaneea.s@gmail.com>
2026-01-11 14:34:30 +0000

    script that converts hbtn_0c_0 database to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -6,7 +6,7 @@ CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-CHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table

2b284059f26dd76b110a402a251414b835b07fd0
shahad-FS <alsaneea.s@gmail.com>
2026-01-13 16:58:22 +0000

    Create database and table script

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- script create database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CRATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL, 
 	PRIMARY KEY (id)

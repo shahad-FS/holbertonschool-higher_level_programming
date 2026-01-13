bd3902aefdbafdfee159066f8a9ebbdd20702aa2
shahad-FS <alsaneea.s@gmail.com>
2026-01-13 18:05:27 +0000

    script that lists all shows and its genre

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- script that lists all shows and all genres
+SELECT s.title, g.name
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS sg
+ON s.id = sg.show_id
+LEFT JOIN tv_genres AS g
+ON sg.genre_id = g.id
+ORDER BY s.title ASC, g.name ASC;

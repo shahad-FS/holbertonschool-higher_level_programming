9df5a1f91effacfeeba914a0b0d5dc5289573f47
shahad-FS <alsaneea.s@gmail.com>
2026-01-16 07:58:52 +0000

    script that lists all cities of given state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[3]
+    state_name = sys.argv[4]
 
     # Connect to the MySQL server
     db = MySQLdb.connect(

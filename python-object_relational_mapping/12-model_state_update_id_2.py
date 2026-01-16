35cd744ce61af5870af2e4dfd39a222bba4dcbde
shahad-FS <alsaneea.s@gmail.com>
2026-01-16 09:43:56 +0000

    script that update the name of state object

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -5,13 +5,13 @@ Changes the name of the State with id = 2 to "New Mexico"
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessoinmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
     # Ensure 3 arguments: username, password, database
-    if len(sys.argv) != 4
-    sys.exit(1)
+    if len(sys.argv) != 4:
+        sys.exit(1)
 
     # Get the credintials from the command-line arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]

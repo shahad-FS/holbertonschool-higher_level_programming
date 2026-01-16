16405362b3d075ce9b06bdc1d4abd556b2f6f097
shahad-FS <alsaneea.s@gmail.com>
2026-01-16 07:45:06 +0000

    lists all cities and their state

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     SELECT cities.id, cities.name, states.name
     FROM cities
     JOIN states
-    ON cities.state_id = state.id
+    ON cities.state_id = states.id
     ORDER BY cities.id ASC
     """
 

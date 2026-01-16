5b992d9b2308fb5f6199bafde5b3ab2f40df2483
shahad-FS <alsaneea.s@gmail.com>
2026-01-16 11:24:31 +0000

    Lists all cities

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -7,6 +7,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_city import City
+from relationship_state import State
 
 
 if __name__ == "__main__":

e4669e4f9f876a19c11ea724fe99938c09d152e8
shahad-FS <alsaneea.s@gmail.com>
2026-01-16 06:59:28 +0000

    lists all states start with 'N'

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,9 +6,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username=sys.argv[1]
-    password=sys.argv[2]
-    database=sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     # Connect to MySQL server
     db = MySQLdb.connect(

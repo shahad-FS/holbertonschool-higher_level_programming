0a28f2d7293b544b3795e1cb5b9e8b99d0dbd6f2
shahad-FS <alsaneea.s@gmail.com>
2026-01-31 09:28:04 +0000

    print message depending of the number of arguments passed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if(process.argv.length > 3){
+if (process.argv.length > 3) {
 	console.log('Arguments found');
-} else if(process.argv.length ===3){
+} else if (process.argv.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('No argument');

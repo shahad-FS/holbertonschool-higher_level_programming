2aaa87fadd232a8ee654a4004751b7a3a9d55cd2
shahad-FS <alsaneea.s@gmail.com>
2026-02-03 13:30:27 +0000

    script that list all movies title from json

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const moveList = document.querySelector('#list_movies');
+
+fetch('https://swapi-api.alx-tools.com/api/films/?format=json')
+  .then((response) => response.json())
+  .then((data) => {
+    for (let index = 0; index < data.results.length; index++) {
+      const NewFilm = document.createElement('li');
+      NewFilm.textContent = data.results[index].title;
+      moveList.appendChild(NewFilm);
+    }
+  });
